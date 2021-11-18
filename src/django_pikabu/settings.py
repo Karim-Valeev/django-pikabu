4c909898d94d01e1cc7c2000eab2ff486055b3bc
Karim-Valeev <karim.valeev.i@gmail.com>
2021-11-18 19:09:34 +0300

    Fix settings for deploy

--- src/django_pikabu/settings.py
+++ src/django_pikabu/settings.py
@@ -137,7 +137,7 @@ LOGIN_URL = "/login/"
 LOGOUT_REDIRECT_URL = "/"
 
 REST_FRAMEWORK = {
-    # "DEFAULT_AUTHENTICATION_CLASSES": ["rest_framework_simplejwt.authentication.JWTAuthentication"],
+    "DEFAULT_AUTHENTICATION_CLASSES": ["rest_framework_simplejwt.authentication.JWTAuthentication"],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
 }
 
