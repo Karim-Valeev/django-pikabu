e30bb85a983f2820906f3f86c0360416cfa8a242
Karim-Valeev <karim.valeev.i@gmail.com>
2021-11-25 04:24:39 +0300

    Add error showing in login template

--- src/main/tests/test_views.py
+++ src/main/tests/test_views.py
@@ -1,6 +1,26 @@
-def inc(x):
-    return x + 1
+import pytest
+from main.models import User
 
 
-def test_inc():
-    assert inc(3) == 4
+@pytest.fixture
+def create_user(db):
+    def make_user():
+        return User.objects.create(email="test@test.com", username="tester", password="test_password")
+
+    return make_user
+
+
+@pytest.fixture
+def auto_login_user(db, client, create_user):
+    def make_auto_login():
+        user = create_user()
+        client.login(email=user.email, password=user.password)
+        return client, user
+
+    return make_auto_login
+
+
+# @pytest.mark.django_db
+# def test_log_in_incorrect_data(client):
+#     response = client.post("/login", {"email": "test@test.test", "password": "incorrect"})
+#     assert response.url == "/sign_in/"
